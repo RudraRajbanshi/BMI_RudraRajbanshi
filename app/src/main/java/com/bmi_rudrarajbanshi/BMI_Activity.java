a7fd2464c1d7ff8de551ac23d2a94807ee8a85bb
Softwarica College Lab <softwarica@softwarica.com>
2019-04-07 11:38:07 +0545

    Initial commit

---
+++ app/src/main/java/com/bmi_rudrarajbanshi/BMI_Activity.java
@@ -0,0 +1,21 @@
+package com.bmi_rudrarajbanshi;
+
+public class BMI_Activity {
+    private double height,weight;
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
